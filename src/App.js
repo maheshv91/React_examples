ac529b115b39dcdb5c5c73947fccba45e2bafcb9
ravikumar34 <varahalaravikumaar3@gmail.com>
2020-03-29 22:27:06 +0530

    about page and router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
+import About from './components/pages/About'
 import axios from 'axios'
 
 import './App.css';
@@ -42,18 +44,30 @@ class App extends Component {
   render() {
     const { users, loading } = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert} />
-          <Users loading={loading} users={users} />
-        </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert} />
+                  <Users loading={loading} users={users} />
 
-      </div>
+                </Fragment>
+              )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+
+          </div>
+
+        </div>
+      </Router>
     );
 
   }
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1> About this App</h1>
+            <p>Search users</p>
+            <p>Version 1.0.0</p>
+
+        </Fragment>
+    )
+}
+export default About
